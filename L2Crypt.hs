22180858b270d71f3f2f48789739007314ae180a
Thanh Do <thanhdo89se@gmail.com>
2019-05-09 18:06:26 +0700

    edit L2Script

--- L2Crypt.hs
+++ L2Crypt.hs
@@ -5,6 +5,9 @@ module L2Crypt where
 import Data.ByteString (ByteString)
 import Data.ByteString.Base16
 import Data.ByteString.Char8
+import qualified Data.ByteArray as ByteArray
+import Data.Word
+
 
 data RecvPacket = RecvPacket {
     _head :: Int,       -- 2 bytes
@@ -23,6 +26,27 @@ first byte indicates the length of packet
 
 -}
 
+{- Cryptic Protocol
+
+1. Login Crypt
+
+Login Server use more complex crypts than game server, but still base on Blowfish Cipher
+
+2. Game Crypt
+
+Server will pick a Blowfish key from a pool (usually 20keys from mobius/acis source)
+
+Crypt Key has a length of 16 bytes (128bit)
+
+-}
+
+-- Blowfish key with the last 8 bytes are static
+-- The first 8 bytes are random-ed from 0 - 255 Int
+keystatic :: ByteArray.Bytes
+keystatic =  ByteArray.pack [0xc8, 0x27, 0x93, 0x01, 0xa1, 0x6c, 0x31, 0x97]
+
+key :: ByteArray.Bytes
+key = ByteArray.pack [0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77] <>  keystatic
 
 nameVerifyReq1 :: ByteString
 -- Request Name: weaverzero (10chars)
