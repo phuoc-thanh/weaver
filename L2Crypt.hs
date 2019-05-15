6e9761333514d34de23d4cea00e594ae3c6de6da
Phuoc-Thanh Do <phuocthanhdo@yahoo.com>
2019-05-15 23:49:01 +0700

    add checksum function - draft

--- L2Crypt.hs
+++ L2Crypt.hs
@@ -4,8 +4,10 @@ module L2Crypt where
 
 import Data.ByteString (ByteString)
 import Data.ByteString.Base16
-import Data.ByteString.Char8
-import qualified Data.ByteArray as ByteArray
+-- import Data.ByteString.Char8
+import Data.Bits
+import Data.ByteArray (Bytes)
+import qualified Data.ByteArray as BArr
 import Data.Word
 
 
@@ -42,11 +44,23 @@ Crypt Key has a length of 16 bytes (128bit)
 
 -- Blowfish key with the last 8 bytes are static
 -- The first 8 bytes are random-ed from 0 - 255 Int
-keystatic :: ByteArray.Bytes
-keystatic =  ByteArray.pack [0xc8, 0x27, 0x93, 0x01, 0xa1, 0x6c, 0x31, 0x97]
+keystatic :: Bytes
+keystatic =  BArr.pack [0xc8, 0x27, 0x93, 0x01, 0xa1, 0x6c, 0x31, 0x97]
 
-key :: ByteArray.Bytes
-key = ByteArray.pack [0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77] <>  keystatic
+key :: Bytes
+key = BArr.pack [0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77] <>  keystatic
+
+
+chksum :: Bytes -> Bool
+chksum bytes
+    | BArr.length bytes     <    5 = False -- check if array size is greater than 4?
+    | BArr.length bytes .&. 3 /= 0 = False -- check if array size is multiple of 4?
+    | otherwise = check bytes
+    where check (x:y:z:w:bs) = (-1 .|. cx .|. cy .|. cz .|. cw) == 0
+          cx = x .&. 0xff
+          cy = y shiftL (0x08 .&. 0xff00)
+          cz = z shiftL (0x10 .&. 0xff0000)
+          cw = w shiftL (0x18 .&. 0xff000000)
 
 nameVerifyReq1 :: ByteString
 -- Request Name: weaverzero (10chars)
