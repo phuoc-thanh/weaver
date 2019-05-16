f225b8624cffd2e537702923db0752958efacc03
Thanh Do <thanhdo89se@gmail.com>
2019-05-16 17:56:32 +0700

    continue checksum fucn

--- L2Crypt.hs
+++ L2Crypt.hs
@@ -50,17 +50,25 @@ keystatic =  BArr.pack [0xc8, 0x27, 0x93, 0x01, 0xa1, 0x6c, 0x31, 0x97]
 key :: Bytes
 key = BArr.pack [0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77] <>  keystatic
 
+zbits = 0x00 :: Word8
 
+-- Checksum function for incoming/outgoing packets
 chksum :: Bytes -> Bool
 chksum bytes
     | BArr.length bytes     <    5 = False -- check if array size is greater than 4?
     | BArr.length bytes .&. 3 /= 0 = False -- check if array size is multiple of 4?
-    | otherwise = check bytes
-    where check (x:y:z:w:bs) = (-1 .|. cx .|. cy .|. cz .|. cw) == 0
-          cx = x .&. 0xff
-          cy = y shiftL (0x08 .&. 0xff00)
-          cz = z shiftL (0x10 .&. 0xff0000)
-          cw = w shiftL (0x18 .&. 0xff000000)
+    | otherwise = (chkw . BArr.unpack $ fst arr) && (chksum $ snd arr)
+    where arr = BArr.splitAt 4 bytes 
+
+-- Check a block of 4 bytes data. If length of input isn't eq to 4, return False
+chkw :: [Word8] -> Bool
+chkw bs@(x:y:z:w:s)
+    | length bs /= 4 = False
+    | otherwise      = cx .|. cy .|. cz .|. cw == zbits where
+      cx = x .&. 0xff
+      cy = y `shiftL` (0x08 .&. 0xff00)
+      cz = z `shiftL` (0x10 .&. 0xff0000)
+      cw = w `shiftL` (0x18 .&. 0xff000000)
 
 nameVerifyReq1 :: ByteString
 -- Request Name: weaverzero (10chars)
