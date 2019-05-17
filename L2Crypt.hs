a46f990d82831931b4be5423088862fbf7be2422
Thanh Do <thanhdo89se@gmail.com>
2019-05-17 16:15:20 +0700

    finish checksum

--- L2Crypt.hs
+++ L2Crypt.hs
@@ -53,22 +53,35 @@ key = BArr.pack [0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77] <>  keystatic
 zbits = 0x00 :: Word8
 
 -- Checksum function for incoming/outgoing packets
+-- Assume that chksum func works with zero offset by default
 chksum :: Bytes -> Bool
 chksum bytes
-    | BArr.length bytes     <    5 = False -- check if array size is greater than 4?
-    | BArr.length bytes .&. 3 /= 0 = False -- check if array size is multiple of 4?
-    | otherwise = (chkw . BArr.unpack $ fst arr) && (chksum $ snd arr)
-    where arr = BArr.splitAt 4 bytes 
+    | len     <    5 = False -- check if array size is greater than 4?
+    | len .&. 3 /= 0 = False -- check if array size is multiple of 4?
+    | otherwise = (xor_fold . fmap chkw $ split_every 4 arr1) == (chkw arr2)
+    where (arr1, arr2) = splitAt (len - 4) words 
+          words        = BArr.unpack bytes
+          len          = BArr.length bytes
 
 -- Check a block of 4 bytes data. If length of input isn't eq to 4, return False
-chkw :: [Word8] -> Bool
+-- Interested: xor 0 x == x
+chkw :: [Word8] -> Word8
 chkw bs@(x:y:z:w:s)
-    | length bs /= 4 = False
-    | otherwise      = cx .|. cy .|. cz .|. cw == zbits where
+    | length bs /= 4 = 0x00
+    | otherwise      = cx .|. cy .|. cz .|. cw where
       cx = x .&. 0xff
       cy = y `shiftL` (0x08 .&. 0xff00)
       cz = z `shiftL` (0x10 .&. 0xff0000)
       cw = w `shiftL` (0x18 .&. 0xff000000)
+
+-- folding with xor return the only non-repeat element in a list
+-- ex: xor_fold [1,2,3,4,3,2,1] should return 4
+-- not sure it is applied in this case
+xor_fold xs = foldr (xor) 0 xs 
+
+-- split a list every n elems
+split_every _ [] = []
+split_every n xs = fs : split_every n rs where (fs, rs) = splitAt n xs
 
 nameVerifyReq1 :: ByteString
 -- Request Name: weaverzero (10chars)
