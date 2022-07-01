b9990357087614bc073fb0d01aea9dd6cf8fdbcb
phuocthanhdo <phuocthanhdo@yahoo.com>
2022-07-01 22:20:06 +0700

    A prompt cmd

--- whoisking/src/main.rs
+++ whoisking/src/main.rs
@@ -2,6 +2,7 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
+use std::io::Write;
 
 use crate::{
     config::{server_addr, with_header, HeaderKind},
@@ -14,12 +15,12 @@ mod login;
 mod packet;
 
 /// Auto write to a pre-config connection stream.
-fn write_stream(credential: Credential) -> Result<(), std::io::Error> {
+fn place_bet(credential: Credential, id: u32, amount: u32) -> Result<(), std::io::Error> {
     let server = server_addr(credential.cfg_server);
 
     let login_msg = with_header(HeaderKind::Login, credential.login());
     let enter_msg = with_header(HeaderKind::Enter, credential.enter_world());
-    let bet_msg = with_header(HeaderKind::Normal, cross_server_war(9000012, 50000));
+    let bet_msg = with_header(HeaderKind::Normal, cross_server_war(id, amount));
 
     let mut stream = TcpStream::connect(server)?;
     println!("Connection established: {:?}", server);
@@ -40,6 +41,15 @@ fn write_stream(credential: Credential) -> Result<(), std::io::Error> {
     Ok(())
 }
 
+fn prompt(name:&str) -> String {
+    let mut line = String::new();
+    print!("{}", name);
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut line).expect("Error: Could not read a line");
+ 
+    return line.trim().to_string()
+}   
+
 fn main() -> Result<(), std::io::Error> {
     // let credentials = Credential::cfg_credentials()?;
 
@@ -47,9 +57,15 @@ fn main() -> Result<(), std::io::Error> {
     //     thread::spawn(|| write_stream(credential));
     // }
 
+    let input = prompt("id:> ");
+    let pid = input.parse::<u32>().unwrap();
+
+    let input = prompt("amount:> ");
+    let amount = input.parse::<u32>().unwrap();
+
     // Single thread
     let credential = Credential::cfg_credential()?;
-    write_stream(credential)?;
+    place_bet(credential, pid, amount)?;
 
     thread::sleep(Duration::from_secs(10));
     println!("Packets sent, please wait 10 secs!");
